5c1c4a01ef2b2ad190e16f6e00d2c6507a5a05a1
phanvu0103 <113657288+phanvu0103@users.noreply.github.com>
2025-02-21 10:24:31 +0700

    Bài 123: Các phương thức về class phần 2, Bài 124: Toggle class

---
+++ .history/JS/08.1 Dom toggle làm trong html css _20250221100459.js
@@ -0,0 +1,11 @@
+//
+// *** *** *** *** ***
+// Bài 124: Toggle class
+console.log("--------- *** Bài 124: Toggle class  *** ---------");
+// làm html css trong index2
+console.log("--------- làm html css trong index2 ---------");
+// menu toggle class
+// const menu = document.querySelector(".menu");
+// menu.classList.add("is-show");
+const menu = document.querySelector(".menu").classList.toggle("is-show");
+// const menu = document.querySelector(".menu").classList.toggle("is-show"); -> nếu thêm dòng nà
